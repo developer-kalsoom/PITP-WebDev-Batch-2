7b8cf5c47b24f330b8a8dac934692f98294eee8b
Jahanzaib <Jahanzaibkhanhyd@example.com>
2025-12-09 16:14:08 +0500

    changes

---
+++ RK/assig.js
@@ -0,0 +1,13 @@
+// let num = 10;
+// let num2 = 5;
+// let num_c= num+num2;
+// console.log(num_c);
+
+x=prompt("enter a word");
+if(x=="that read from both sides");
+alert("palindrome words")
+
+
+{else("not palindrome");
+
+}
